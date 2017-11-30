6131ff9c8e8d31dd33d62df9c511f1d27dea2ee6
Kamalesh Das <Kamaleshdas2011@outlook.com>
2017-11-30 11:47:08 +0000

    angular added

---
+++ kd-aspmvc/Scripts/Javascripts/ImageController.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var app = angular.module("app");
+
+    var ImageController = function ($scope) {
+        $scope.message = "new angular";
+    }
+
+    app.controller("ImageController", ["$scope", ImageController]);
+}());
